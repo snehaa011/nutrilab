9e7fc3c424fed335a7e1e29962662bc39f903f52
snehaa011 <snehakurupofficial@gmail.com>
2024-05-09 17:10:06 +0530

    mod

--- lib/navbarwidget.dart
+++ lib/navbarwidget.dart
@@ -1,73 +1,88 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:nutrilab/menupage.dart';
 import 'package:nutrilab/planpage.dart';
 
-Widget buildnavbar(BuildContext context) {
-  return BottomAppBar(
-    color: Color.fromARGB(255, 226, 226, 226),
-    padding: EdgeInsets.all(0),
-    child: Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20),
-          topRight: Radius.circular(20),
-        ),
-        color: Color.fromARGB(255, 24, 79, 87),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Spacer(),
-          IconButton(
-            onPressed: () {},
+class BottomNav extends StatefulWidget {
+  const BottomNav({super.key});
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  int myIndex=0;
+  List<Widget> pages=[
+    GoToMenuPage(),
+    GoToPlanPage(),
+    GoToMenuPage(),
+    GoToPlanPage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 226, 226, 226),
+      body: pages[myIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        // showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Color.fromARGB(255, 253, 253, 253),
+        unselectedItemColor: Colors.white,
+        iconSize: 35,
+        backgroundColor: Color.fromARGB(255, 24, 79, 87),
+        onTap: (index) {
+          setState(() {
+            myIndex=index;
+          });
+        },
+        currentIndex: myIndex,
+        items: [
+          BottomNavigationBarItem(
             icon: Icon(
               Icons.local_dining,
-              color: Colors.white,
-              size: 30,
+              // color: Colors.white,
+              // size: 30,
             ),
+            label: 'Menu',
           ),
-          Spacer(
-            flex: 2,
-          ),
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => GoToPlanPage(),
-                ),
-              );
-            },
+          BottomNavigationBarItem(
             icon: Icon(
               Icons.shopping_bag,
-              color: Colors.white,
+              // color: Colors.white,
               size: 30,
             ),
+            label: 'Mealplans',
           ),
-          Spacer(
-            flex: 2,
-          ),
-          IconButton(
-            onPressed: () {},
+          BottomNavigationBarItem(
             icon: Icon(
               Icons.menu_book_outlined,
-              color: Colors.white,
+              // color: Colors.white,
               size: 30,
             ),
+            label: 'Cookbook',
           ),
-          Spacer(
-            flex: 2,
-          ),
-          IconButton(
-            onPressed: () {},
+          BottomNavigationBarItem(
             icon: Icon(
               Icons.person_sharp,
-              color: Colors.white,
+              // color: Colors.white,
               size: 30,
             ),
+            label:'Profile',
           ),
-          Spacer(),
         ],
+      
+        // child: Container(
+        //   decoration: BoxDecoration(
+        //     borderRadius: BorderRadius.only(
+        //       topLeft: Radius.circular(20),
+        //       topRight: Radius.circular(20),
+        //     ),
+        //     color: Color.fromARGB(255, 24, 79, 87),
+        //   ),
+        // ),
       ),
-    ),
-  );
+    );
+  }
 }
