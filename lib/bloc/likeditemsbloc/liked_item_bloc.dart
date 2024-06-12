16fe7af2c77b4e8507151ed62d58b93953cf1be7
Sneha <sneha.biju@talabat.com>
2024-06-12 12:27:08 +0400

    added event and states for liked item bloc

---
+++ lib/bloc/likeditemsbloc/liked_item_bloc.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+
+import './liked_item_event.dart';
+import './liked_item_states.dart';
+
+class LikedItemBloc extends Bloc< LikedItemEvent, LikedItemState> {
+  
+}
